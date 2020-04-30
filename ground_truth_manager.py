efb26c6d40a5b542621bd472d8ae61db1cc8f126
mmangione <martina.mangione-cic-it@ibm.com>
2020-04-30 14:56:28 +0200

    add random generator for testing ground truth

--- ground_truth_manager.py
+++ ground_truth_manager.py
@@ -1,6 +1,7 @@
 import pandas as pd
+from py_thesaurus import Thesaurus
 
-newCluster = [] #Nuovo cluster da costruire e riempire
+newCluster = [] #Nuovo cluster da costruire e riempire. E' un lista di tuple
 cluster = []
 #Prendo la lista di cluster generata nella fase precedente miniclusterRaggruppato.txt
 # Ogni lista in miniclusterraggruppato rappresenta un prodotto. All'interno di quel prodotto ha una lista di tuple che
@@ -8,36 +9,64 @@ cluster = []
 with open("miniClusterRaggruppato.txt", "r") as file:
     cluster = eval(file.readline())
 
-# Leggi la ground truth in un Pandas dataframe
-df = pd.read_csv('ground_truth/instance_attributes_gt.csv')
+df = pd.read_csv("ground_truth/ground_truth_random_reducedx2.csv")
 
 # Scorro solo le coppie match
-for index, row in df[df['label'] == 1].iterrows():
+for index, row in df.iterrows():
    target_attribute = row['left_target_attribute']
+   # Filtraggio della ground truth prima dell'esecuzione dell'algoritmo
+   # Si prendono tutte le righe con lo stesso target attribute. Si scartano tutti quelli che hanno left_attribute e right_attribute
+   # 1 SI SCORRE TUTTA LA GROUND TRUTH E SI CREANO DEI CLUSTER CON I SOLI ELEMENTI CHE LA COMPONGONO
+   # 2 SI UNISCONO I CLUSTER PRECEDENTEMENTE CREATI DI miniclusterRaggruppato.txt
    left_attribute = row['left_instance_attribute']
    right_attribute = row['right_instance_attribute']
-   for product in cluster:
-       # product è una lista di tuple con gli attributi del prodotto
-       for attributes in product: #Salto la posizione zero che è solo il nome del cluster
-            attribute = attributes[1] # tupla con gli attributi
-            for tupla in attribute:
-                if sameCluster(target_attribute,left_attribute,right_attribute,tupla):
-                    #Aggiungo al nuovo cluster
-           #Quando si vuole aggiungere un nuovo elemento al cluster occorre controllare che non sia già presente
+   left_value = row['left_instance_value']
+   right_value = row['right_instance_value']
+   ldata = left_attribute.split("//")[2]
+   rdata = right_attribute.split("//")[2]
+   attributeNameList = set([ldata, rdata])
+   fileNameList = [left_attribute, right_attribute]
+   attributeValueList = set([left_value, right_value])
+   if any(target_attribute in d for d in newCluster):
+       for dictionary in newCluster:
+           if target_attribute in dictionary.keys():
+               #Dal momento che i valori di un dizionario non sono iterabili
+               valori = list(dictionary.values())[0]
+               # Uso un set così da scartare i duplicati
+               newAttributeNameList = set().union(valori[0], attributeNameList)
+               newAttributeValueList = set().union(valori[1], attributeValueList)
+               newfileNameList = valori[2] + fileNameList
+               dictionary[target_attribute] = (newAttributeNameList, newAttributeValueList, newfileNameList)
+   else:
+       newCluster.append({target_attribute: (attributeNameList, attributeValueList, fileNameList)})
+
+# Sono stati creati dei cluster con la ground_truth. Adesso possiamo unire questi cluster con quelli creati nella fase precedente
+for product in cluster:
+    # product è una lista di tuple con gli attributi del prodotto
+    for attributes in product: #Salto la posizione zero che è solo il nome del cluster
+        attribute = attributes[1] # tupla con gli attributi
+        for tupla in attribute:
+            print('ciao')
 
 
 
-def sameCluster(target_attribute,left_attribute,right_attribute,*tupla):
-    ldata = left_attribute.split("/")
-    lattribute = ldata[2]
-    rdata = right_attribute.split("/")
-    rattribute = rdata[2]
-    if target_attribute == tupla[0] or synonimous(target_attribute, tupla[0]):
-       #TODO: Da finire da implementare
-        return True
+
+#def sameCluster(target_attribute,left_attribute,right_attribute,*tupla):
+#    ldata = left_attribute.split("/")
+#    lattribute = ldata[2]
+#    rdata = right_attribute.split("/")
+#    rattribute = rdata[2]
+ #   if target_attribute == tupla[0] or synonimous(target_attribute, tupla[0]):
+#       #TODO: Da finire da implementare
+ #       return True
 
 
 
-def synonimous(attribute_name, target_atribute):
-    #TODO: Da implementare il dizionario di sinonimi
-    return True
+#def synonimous(attribute_name, target_atribute):
+#    target_synonims = Thesaurus(target_attribute)
+#    synonims = (target_synonims.get_synonym())
+#    if attribute_name in synonims:
+#        return True
+#    else #Si cerca nel dizionario dei sinonimi
+#    #TODO: Da implementare il dizionario di sinonimi
+#    return True
---
+++ ground_truth_sampler.py
@@ -0,0 +1,29 @@
+import random
+import pandas as pd
+
+
+#Questo file è stato creato per generare una ground truth più piccola e velocizzare la fase di sviluppo
+#Questa operazione è molto lenta. Il file csv viene salvato tutto in memoria per prendere solo le righe che hanno label=1
+
+# TODO: Trovare un criterio per cui estrarre gli esempi più importanti
+# SOLUZIONE TEMPORANEA: Sample random dei dati per velocizzare la computazione
+#df = pd.read_csv("ground_truth/instance_attributes_gt.csv")
+#dati = df[df['label'] == 1]
+#dati.sample(n=5)
+#dati.to_csv(r'ground_truth/ground_truth_random_5.csv', index=False, header=True)
+
+
+
+#Secondo approccio più veloce
+# The data to load
+f = "ground_truth/instance_attributes_gt.csv"
+# Considero ogni 20000 righe
+n = 20000
+num_lines = sum(1 for l in open(f))
+# Salto l'header
+skip_idx = [x for x in range(1, num_lines) if x % n != 0]
+# Leggo i dati e prendo solo quelli con label=1
+data = pd.read_csv(f, skiprows=skip_idx)
+dati = data[data['label'] == 1]
+dati.to_csv(r'ground_truth/ground_truth_random_reducedx2.csv', index=False, header=True)
+print("FATTO")
