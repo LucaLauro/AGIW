115012e60a50bb1defedd4af919f11ff5296e9c3
Luca <luca@MBP-di-Luca.lan>
2020-05-04 13:36:29 +0200

    modificato metodo accettabili

--- mergeByName.py
+++ mergeByName.py
@@ -10,10 +10,12 @@ def accettabili(val1, val2):
     sim = fuzz.token_sort_ratio(val1, val2)
 
     sim2=fuzz.token_set_ratio(val1,val2)
-    if sim >39 and sim<51 and sim2<80 and sim2>50:
-        print(sim)
-        print(fuzz.token_set_ratio(val1,val2))
-    return sim >39 and sim<51 and sim2<80 and sim2>50
+    if sim>50 :
+        return True
+    if sim>35 and sim < 50 and len(val1)>3 and len(val2)>3 and sim2>50:
+        return True
+    return False
+    
 
 
 def merge_name_product(dict_product, cluster_product):
@@ -22,7 +24,7 @@ def merge_name_product(dict_product, cluster_product):
         for key1 in dict_name_attribute:
             for _, d in dict_product:
                 for key2 in d:
-                    if (dict_name_attribute[key1] / d[key2]) > 0 and fuzz.token_set_ratio(key1, key2) > 90:
+                    if fuzz.token_set_ratio(key1, key2) > 90:
                         bind += [(key1, key2)]  # a1-a5, a4
 
     i = 0
@@ -77,4 +79,5 @@ N = 188
 merge_name_product(prova_cluster()[N], miniCluster[N])
 
 #soglia da cambiare
-#minimo sort 50, se sort tra 35 e 50 -> se set è maggiore di 70/80 allora va bene (spero non caghi errori) forse anche 50 ma devo fare i controlli e togliere parole più corte di 4 caratteri
+#minimo sort 50, se sort tra 35 e 50 -> se set è maggiore di 70/80 allora va bene (spero non caghi errori) forse anche 50 ma devo fare i controll
+# i e togliere parole più corte di 4 caratteri
