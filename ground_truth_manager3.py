029072d47c090b44710fe8a7d76a4d3834c3f3d1
mmangione <martina.mangione-cic-it@ibm.com>
2020-05-06 09:26:48 +0200

    check only the attribute of the first tuple

--- ground_truth_manager3.py
+++ ground_truth_manager3.py
@@ -24,7 +24,7 @@ cluster = []
 #Prendo la lista di cluster generata nella fase precedente miniclusterRaggruppato.txt
 # Ogni lista in miniclusterraggruppato rappresenta un prodotto. All'interno di quel prodotto ha una lista di tuple che
 # rappresentano gli attributi di quel prodotto
-with open("test_files/product_cluster_for_ground_truth", "r") as file:
+with open("miniClusterPassata4.txt", "r") as file:
     cluster = eval(file.readline())
 df = pd.read_csv("ground_truth/ground_truth_random_reducedx2.csv")
 
